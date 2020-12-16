34d6b32bf1ea6f6a3209430897b75860563d2f61
AndyTheCruckl <andyboce@hotmail.com>
2020-12-15 20:29:56 -0600

    provocando errores

--- funciones.js
+++ funciones.js
@@ -2,4 +2,17 @@ function concatenar (texto1, texto2) {
     console.log(texto1 + ' ' + texto2);
 }
 
+
+let multiplicar = function multiplicar (num1,num2){
+    if(num1 == 0 || num2 == 0){
+        return 0;
+    }else{
+        return num1 * num2;
+    }
+} 
+
+function tonteando (){
+    console.log("simplemente generando errores");
+}
+
 module.exports = concatenar;
